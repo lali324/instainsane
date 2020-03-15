d14d9e17e6813a696d613469c68f29befb4acf2d
magtava <62004978+magtava@users.noreply.github.com>
2020-03-15 01:42:46 -0300

    Update instainsane.sh

--- instainsane.sh
+++ instainsane.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Coded by: thelinuxchoice (Don't change, noob!)
 # www.github.com/thelinuxchoice
-# www.instagram.com/linux_choice
+# www.instagram.com/linux_choicechmod +x instainsane.sh
 
 trap 'store; exit 1;' 2
 
